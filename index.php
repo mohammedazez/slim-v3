49d26cac549a683b4e2faa3ec5fb63beb7c5adff
mohammedazez <muhamadaziz047@gmail.com>
2021-11-28 14:02:33 +0700

    param url route

--- index.php
+++ index.php
@@ -3,12 +3,16 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\App();
 
-$app -> get('/', function($request, $response, $args){
+$app -> get('/', function($request, $response){
     return $response->withStatus(200)->write("Hello world");
 });
 
-$app -> get('/forum', function($request, $response, $args){
+$app -> get('/forum', function($request, $response){
     return $response->withStatus(200)->write("This a forum");
+});
+
+$app -> get('/judul[/{nama}/{halaman}]', function($request, $response, $args){
+        return $args['nama'] . ' di halaman ' . $args['halaman'];
 });
 
 $app -> run();
