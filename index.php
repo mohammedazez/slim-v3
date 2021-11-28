90e5d50ccaf94ea6f21fc99c9f1d56819a11965b
mohammedazez <muhamadaziz047@gmail.com>
2021-11-28 14:58:51 +0700

    route twig

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true
+    ]
+]);
 
 $app -> get('/', function($request, $response){
     return $response->withStatus(200)->write("Hello world");
@@ -14,5 +18,40 @@ $app -> get('/forum', function($request, $response){
 $app -> get('/judul[/{nama}/{halaman}]', function($request, $response, $args){
         return $args['nama'] . ' di halaman ' . $args['halaman'];
 });
+
+
+$app -> get('/name', function($request, $response){
+    $data = array(
+        'nama' => 'Muhamad Aziz',
+        'hobby' => 'Making Artificial Intel'
+    );
+
+    return $response -> withJson($data, 200);
+});
+
+
+// Get container
+$container = $app->getContainer();
+
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig( __DIR__. '/templates', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
+// Render Twig template in route
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
 
 $app -> run();
